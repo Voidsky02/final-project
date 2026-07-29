fac54adba7b1113f8807a7f74f8839b85660c189
voidsky02 <189816457+Voidsky02@users.noreply.github.com>
2026-07-29 01:04:54 -0700

    useForm integrated into RegisterModal successfully, tracks input form values in state correctly

--- frontend/src/components/RegisterModal/RegisterModal.jsx
+++ frontend/src/components/RegisterModal/RegisterModal.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import './RegisterModal.css';
 import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
-//! Import useForm - All universal form functionality ???
+import useForm from "../../hooks/useForm.js";
 //! Import useModal - All universal modal functionality ???
 
 // For Profile creation.
@@ -9,12 +9,16 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 //! Recieve useModal hook functions and variables as props from <Laytout />
 function RegisterModal({ isOpen, closeModal, handleOffModalClick }) {
 
+    const { values, handleChange, resetForm } = useForm({ username: "", email: "", password: "", confirmPassword: "" });
+
     // handleSubmit is the only function created in the custom modal itself
     // because the submit logic is different depending on the modal
-    function handleSubmit(event) {
+    function handleSubmit(event) { //! Finish this function
         // In previous project i just prevented default behavior,
         // then called a custom onSignUp function in here and thats it.
         event.preventDefault();
+        //! More ????
+        resetForm();
     }
 
     return (
@@ -37,20 +41,64 @@ function RegisterModal({ isOpen, closeModal, handleOffModalClick }) {
         / When the form is submitted, the browser packages the data using the name as the key. */}
 
         <div className="register__form-field">
-            <label className="register__label" htmlFor="username">Username</label>
-            <input className="register__input" type="string" id="username" name="username" value={""} onChange={""} placeholder="Enter username here" ></input>
+            <label className="register__label" htmlFor="username">
+                Username
+            </label>
+            <input
+                className="register__input"
+                type="string"
+                id="username"
+                name="username"
+                value={values.username}
+                onChange={handleChange}
+                placeholder="Enter username here"
+                required
+            />
         </div>
         <div className="register__form-field">
-            <label className="register__label" htmlFor="email">Email</label>
-            <input className="register__input" type="email" id="email" name="email" value={""} onChange={""} placeholder="Enter email here" ></input>
+            <label className="register__label" htmlFor="email">
+                Email
+            </label>
+            <input
+                className="register__input"
+                type="email"
+                id="email"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                placeholder="Enter email here"
+                required
+            />
         </div>
         <div className="register__form-field">
-            <label className="register__label" htmlFor="password">Password</label>
-            <input className="register__input" type="string" id="password" name="password" value={""} onChange={""} placeholder="Enter password here" ></input>
+            <label className="register__label" htmlFor="password">
+                Password
+            </label>
+            <input
+                className="register__input"
+                type="string"
+                id="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                placeholder="Enter password here"
+                required
+            />
         </div>
         <div className="register__form-field">
-            <label className="register__label" htmlFor="confirmPassword">Confirm Password</label>
-            <input className="register__input" type="string" id="confirmPassword" name="confirmPassword" value={""} onChange={""} placeholder="Confirm your password here" ></input>
+            <label className="register__label" htmlFor="confirmPassword">
+                Confirm Password
+            </label>
+            <input
+                className="register__input"
+                type="string"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={values.confirmPassword}
+                onChange={handleChange}
+                placeholder="Confirm your password here"
+                required
+            />
         </div>
     </ModalWithForm>
     )
--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -1,1 +1,36 @@
 // After i implement the form functionality itself
+import { useState, useEffect } from "react";
+
+// input state management: https://www.youtube.com/watch?v=IkMND33x0qQ
+// How to submit those states: https://www.youtube.com/watch?v=pJiRj02PkJQ
+
+function useForm(initialValues, prefillData = null) {
+    const [values, setValues] = useState(() => prefillData || initialValues); //! Cant use useEffect cause i get an error about setting state syncronously so have to use this new method, but it doesnt update when prefillData changes
+
+    //! Old method no longer working
+    // useEffect(() => {
+    //     if (prefillData) {
+    //         setValues(prefillData);
+    //     }
+    // }, [prefillData])
+
+    // Assign input to value of state variable.
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value });
+        console.log(`${name}: ${value}`);
+    }
+
+    // Reset form function to original states.
+    function resetForm() {
+        setValues(initialValues);
+    }
+
+    return {
+        values,
+        handleChange,
+        resetForm,
+    };
+}
+
+export default useForm;
