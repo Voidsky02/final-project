047085dfef61e065210886f3222c1da1bcb25819
voidsky02 <189816457+Voidsky02@users.noreply.github.com>
2026-07-29 01:04:54 -0700

    add clunky joi validation with missing celebrate express middleware implementation

--- backend/src/middlewares/validation.js
+++ backend/src/middlewares/validation.js
@@ -1,2 +1,23 @@
 /* Joi & Celebrate validation for user password input */
 
+const registerSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).trim().required().messages({
+        'any.required': 'Username field cannot be left blank',
+        'string.min': 'Username must be longer than 3 characters',
+        'string.max': 'Username must be shorter than 30 characters',
+        'string.alphanum': 'Username must only include letters and numbers',
+
+    }),
+    email: Joi.string().email().trim().required().messages({
+        'any.required': 'Email field cannot be left blank',
+        'string.email': 'Input must be a valid email'
+    }),
+    password: Joi.string().min(8).pattern(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/).required().messages({
+        'any.required': 'Password field cannot be left blank',
+        'string.pattern.base': 'Password must include at least one uppercase & lowercase letter and at least one number'
+    }),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+        'any.required': 'Confirm password field cannot be left blank',
+        'any.only': 'Passwords do not match',
+    }),
+});
