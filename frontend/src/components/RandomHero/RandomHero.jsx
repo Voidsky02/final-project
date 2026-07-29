0e89add34f46f412ffb3ae74a7036cdb1e017c64
voidsky02 <189816457+Voidsky02@users.noreply.github.com>
2026-07-29 01:04:53 -0700

    create starting template for randomHero jsx, css, and api hook

---
+++ frontend/src/components/RandomHero/RandomHero.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import './RandomHero.css';
+
+function RandomHero() {
+    // state variables
+
+    // useEffect (what to do on first render)
+
+    // return the react component (html basically)
+}
+
+export default RandomHero;
