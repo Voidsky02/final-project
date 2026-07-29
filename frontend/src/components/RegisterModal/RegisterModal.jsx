0146cfb20dc3990db4b798a1cb54e34d42f54eb4
voidsky02 <189816457+Voidsky02@users.noreply.github.com>
2026-07-29 01:04:54 -0700

    added all the labels and inputs to register form, and basic roughdraft styles

--- frontend/src/components/RegisterModal/RegisterModal.jsx
+++ frontend/src/components/RegisterModal/RegisterModal.jsx
@@ -29,14 +29,29 @@ function RegisterModal({ isOpen, closeModal, handleOffModalClick }) {
         //! Below is created inside this component
         handleSubmit={""}
     >
-        // * Form elements for creating a profile for first time
-        // * (Username, Email, Password, confirm password)
-        // *
-        // * htmlFor= and id= are what link the label and input together
-        // * The name attribute is for form data submission and state management in React.
-        // * When the form is submitted, the browser packages the data using the name as the key.
-        <label htmlFor="username">Username</label>
-        <input type="string" id="username" name="username" value={} onChange={} ></input>
+        {/* Form elements for creating a profile for first time
+        / (Username, Email, Password, confirm password)
+        /ther
+        / The name attribute is for form data submission and state management in React.
+        / htmlFor= and id= are what link the label and input together
+        / When the form is submitted, the browser packages the data using the name as the key. */}
+
+        <div className="register__form-field">
+            <label className="register__label" htmlFor="username">Username</label>
+            <input className="register__input" type="string" id="username" name="username" value={""} onChange={""} placeholder="Enter username here" ></input>
+        </div>
+        <div className="register__form-field">
+            <label className="register__label" htmlFor="email">Email</label>
+            <input className="register__input" type="email" id="email" name="email" value={""} onChange={""} placeholder="Enter email here" ></input>
+        </div>
+        <div className="register__form-field">
+            <label className="register__label" htmlFor="password">Password</label>
+            <input className="register__input" type="string" id="password" name="password" value={""} onChange={""} placeholder="Enter password here" ></input>
+        </div>
+        <div className="register__form-field">
+            <label className="register__label" htmlFor="confirmPassword">Confirm Password</label>
+            <input className="register__input" type="string" id="confirmPassword" name="confirmPassword" value={""} onChange={""} placeholder="Confirm your password here" ></input>
+        </div>
     </ModalWithForm>
     )
 }
